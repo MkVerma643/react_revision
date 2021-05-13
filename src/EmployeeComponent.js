5f7bf9423a5e7bdc4771a80cf31a33d1d729c24b
MkVerma643 <mukeshverma643@gmail.com>
2021-05-13 13:55:37 +0530

    User Admin and Employee Component

---
+++ src/EmployeeComponent.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react'
+
+class EmployeeComponent extends Component {
+    constructor(props) {
+        super(props)
+    
+        this.state = {
+             
+        }
+    }
+    
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
+
+export default EmployeeComponent
---
+++ src/UserComponent.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react'
+
+class UserComponent extends Component {
+    constructor(props) {
+        super(props)
+    
+        this.state = {
+             user_name: "",
+             user_number: ""
+        }
+    }
+    
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
+
+export default UserComponent
